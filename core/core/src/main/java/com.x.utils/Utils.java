8c3260beeef54071f46b33689db660ef70cdd8fa
fupengbo <fupengbo@188yd.com>
2018-12-18 11:14:28 +0800

    增加常用判断

--- core/core/src/main/java/com.x.utils/Utils.java
+++ core/core/src/main/java/com.x.utils/Utils.java
@@ -3,6 +3,9 @@ package com.x.utils;
 import com.x.contract.ErrorCodes;
 import com.x.contract.ServiceException;
 
+import java.math.BigDecimal;
+import java.util.Collection;
+
 /**
  * Created by fpb on 2018/12/17.
  */
@@ -24,7 +27,97 @@ public class Utils {
     public static ServiceException getServiceException(int errCode, String message, Throwable throwable) {
         return new ServiceException(message,errCode,throwable);
     }
+    /**
+     * 判断当前BigDecimal值是否大于0
+     * @param value
+     * @return
+     */
+    public static Boolean isBigThanZero(BigDecimal value){
+        if(value == null){
+            return false;
+        }
+        if(value.doubleValue() > 0.0){
+            return true;
+        }
+        return false;
+    }
 
+    /**
+     * 是空的字符串
+     * @param value
+     * @return
+     */
+    public static Boolean isEmptyString(String value){
+        return value == "" || value == null || value.length() <= 0;
+    }
+    /**
+     * 不是空的字符串
+     * @param value
+     * @return
+     */
+    public static Boolean isNotEmptyString (String value){
+        return !isEmptyString(value);
+    }
+    /**
+     * 是空集合
+     * @param collection
+     * @param <E>
+     * @return
+     */
+    public static <E> Boolean isEmptyCollection(Collection<E> collection){
+        return collection == null || collection.size() == 0;
+    }
 
+    /**
+     * 不是空集合
+     * @param collection
+     * @param <E>
+     * @return
+     */
+    public static <E> Boolean isNotEmtyCollection(Collection<E> collection){
+        return !isEmptyCollection(collection);
+    }
+    /**
+     * 取出集合中第一个元素
+     * @param collection
+     * @param <E>
+     * @return
+     */
+    public static <E> E firstCollection(Collection<E> collection){
+        if(isEmptyCollection(collection)){
+            return null;
+        }else{
+            return collection.iterator().next();
+        }
+    }
+    /**
+     * 是中国电话号码
+     * @param phoneNumber
+     * @return
+     */
+    public static Boolean isMobilePhoneInChina(String phoneNumber){
+        if(isEmptyString(phoneNumber)){
+            return false;
+        }
+        return phoneNumber.matches("^(13[0-9]|14[0-9]|16[0-9]|17[0-9]|15[0-9]|18[0-9]|19[0-9])\\d{8}$");
+    }
+    /**
+     * 电话号码格式化
+     * @param mobliePhoneNumber
+     * @return
+     */
+    public static String encodeMobliePhoneNumber(String mobliePhoneNumber){
+        if(isEmptyString(mobliePhoneNumber)){
+            return "";
+        }
+        if(!isMobilePhoneInChina(mobliePhoneNumber)){
+            return mobliePhoneNumber;
+        }
+        StringBuilder sb = new StringBuilder(mobliePhoneNumber);
+        for (int i = 3; i <= 6; i++) {
+            sb.setCharAt(i, '*');
+        }
+        return sb.toString();
+    }
 
 }
